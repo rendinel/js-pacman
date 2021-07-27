f10e9679e65e6091c077e7548663f8752ff50893
rendinel <rendinel@protonmail.com>
2021-07-27 23:17:38 +0200

    push

--- app.js
+++ app.js
@@ -41,11 +41,22 @@ document.addEventListener('DomContentLoaded', () => {
       // 3 - power-pellet
       // 4 - empty
 
-      function createBord() {
+      //drwa grid and render it
+      function createBoard() {
           for (let i = 0; i < layout.length; i++){
               const square = document.createElement('div')
               grid.appendChild(square)
               squares.push(square)
+
+              //add layout to the board
+              if(layout[i] === 0){
+                  squares[i].classList.add('pac-dot')
+              } else if(layout[i] === 1){
+                  squares[i].classList.add('wall')
+              } else if(layout[i] === 3){
+                  squares[i].classList.add('power-pellet')
+              }
           }
-      }
+        }
+        createBoard()
 })
